47ef6d824bdf138d000603adfa77eb5e5a6d1a8a
Isaac Mendoza <alcalamendoza5@gmail.com>
2025-05-15 15:08:35 -0300

    primer test

---
+++ JS/dropdown.js
@@ -0,0 +1,13 @@
+// Script para rolagem suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
